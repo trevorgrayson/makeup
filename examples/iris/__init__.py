571a972385bd3abf2d3aecfa1ee7645ff882dfd3
Trevor Grayson <trevor@trevorgrayson.com>
2021-06-08 00:51:34 -0700

    iris model going generic

--- examples/iris/__init__.py
+++ examples/iris/__init__.py
@@ -17,44 +17,31 @@ def load():
     return iris_X, iris_y
 
 
-def features(iris_X, iris_y):
+def features(X, y):
     """Optional method to transform data, add or remove columns"""
     # if i had pandas here, I would do something!
-    return iris_X, iris_y
+    return X, y
 
 
-def split(iris_X, iris_y):
+def split(X, y):
     np.random.seed(0)
-    indices = np.random.permutation(len(iris_X))
-    iris_X_train = iris_X[indices[:-10]]
-    iris_y_train = iris_y[indices[:-10]]
-    iris_X_test = iris_X[indices[-10:]]
-    iris_y_test = iris_y[indices[-10:]]
+    indices = np.random.permutation(len(X))
+    X_train = X[indices[:-10]]
+    y_train = y[indices[:-10]]
+    X_test = X[indices[-10:]]
+    y_test = y[indices[-10:]]
 
-    return (iris_X_train, iris_y_train),\
-           (iris_X_test, iris_y_test)
+    return (X_train, y_train),\
+           (X_test, y_test)
 
 
-def train(iris_X, iris_y):
-     # Create and fit a nearest-neighbor classifier
+def train(X, y):
+    """Create and fit a nearest-neighbor classifier"""
     model = KNeighborsClassifier()
-    model.fit(iris_X, iris_y)
+    model.fit(X, y)
 
     return model
 
 
 def predict(model, X):
     return model.predict(X)
-
-
-# I propose the following would be handled by a framework
-
-data = load()
-X, y = features(*data)
-train_data, test_data = split(X, y)
-test_X, test_y = test_data
-model = train(*train_data)
-print(
-    predict(model, test_X)
-)
-
