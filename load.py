d070cd84331ea920c4f139182543b34bfc1a0e57
Trevor Grayson <trevor@trevorgrayson.com>
2021-06-08 01:04:08 -0700

    clean up

--- load.py
+++
@@ -1,9 +0,0 @@
-def purchases():
-    return [(i//2, 'item%s'%i) for i in xrange(1,10)]
-
-def accounts():
-    return [(i, 'bob%s@gmail.com'%i) for i in xrange(0,5)]
-
-# def accounts():
-#     for i in xrange(1,500):
-#         yield (i, 'bob%s@gmail.com'%i) 
